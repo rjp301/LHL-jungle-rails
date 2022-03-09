b9f5a61fdb4471aecbf1e633a161b27e5265e2b9
rjp301 <rileypaul96@gmail.com>
2022-03-09 15:46:45 -0700

    Functionality complete

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["HTTP_ADMIN_USER"], password: ENV["HTTP_ADMIN_PASS"]
 
   def show
+    @num_products = Product.all.count
+    @num_categories = Category.all.count
   end
 end
